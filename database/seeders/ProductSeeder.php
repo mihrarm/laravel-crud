b37d0c5eb1f85bbbe6ef15921fb7a89cdfc64ad3
mihrarm <mher.safaryan.94@gmail.com>
2022-06-24 01:01:21 +0400

    first commit

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $product = new Product([
+            'name' => 'Test product',
+            'detail' => 'Test product details'
+        ]);
+        $product->save();
+    }
+}
